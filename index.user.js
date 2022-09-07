b67cfc478c94528d14bcd255d04fed1662ca8a64
k.golikov <kgolikov@37bytes.com>
2022-09-07 18:45:24 +0500

    Handle errors

--- index.user.js
+++ index.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Removes the blocking banner showing to unauthorized users on Reverso Context
+// @name         Remove the blocking banner showing to unauthorized users on Reverso Context
 // @namespace    https://context.reverso.net
 // @version      1.0
 // @author       MRGRD56
@@ -11,26 +11,31 @@
 (function () {
     const EXTENSION_NAME = 'mrgrd56/reverso-context-unblocker';
 
-    document.getElementById('blocked-results-banner')?.remove();
-    document.getElementById('blocked-rude-results-banner')?.remove();
+    try {
+        document.getElementById('blocked-results-banner')?.remove();
+        document.getElementById('blocked-rude-results-banner')?.remove();
 
-    const examplesContainer = document.getElementById('examples-content');
-    if (!examplesContainer) {
-        console.error(`[${EXTENSION_NAME}] unable to find '#examples-content' element, so could not show and unblur the hidden examples`);
-        return;
-    }
-
-    const examples = examplesContainer.querySelectorAll('.example.blocked');
-    if (!examples.length) {
-        console.error(`[${EXTENSION_NAME}] unable to find '.example.blocked' elements, so could not show and unblur the hidden examples`);
-        return;
-    }
-
-    examples.forEach(example => {
-        example.classList.remove('blocked');
-        example.style.display = null;
-        if (example.getAttribute('style') === '') {
-            example.removeAttribute('style');
+        const examplesContainer = document.getElementById('examples-content');
+        if (!examplesContainer) {
+            return;
         }
-    });
+
+        const examples = examplesContainer.querySelectorAll('.example.blocked');
+        if (!examples.length) {
+            return;
+        }
+
+        examples.forEach(example => {
+            example.classList.remove('blocked');
+            if (example.display === 'none') {
+                example.style.display = null;
+            }
+            if (example.getAttribute('style') === '') {
+                example.removeAttribute('style');
+            }
+        });
+    } catch (e) {
+        console.error(`An error occurred in script '${EXTENSION_NAME}'`)
+        throw e;
+    }
 })();
