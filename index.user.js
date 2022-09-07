110f0d0cdf2496d50b99321286d45fa6f8cc160f
k.golikov <kgolikov@37bytes.com>
2022-09-07 13:22:40 +0500

    Initial commit

---
+++ index.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Remove blocking banner showing to unauthorized users from Reverso Context
+// @namespace    https://context.reverso.net
+// @version      1.0
+// @author       MRGRD56
+// @match        https://context.reverso.net*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
+
+// TODO remove the banner, show hidden and blurred elements
