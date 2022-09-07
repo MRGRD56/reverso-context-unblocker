4712937387345ec7b94f7406a9769ac5f3128595
k.golikov <kgolikov@37bytes.com>
2022-09-07 18:58:37 +0500

    Fix

--- index.user.js
+++ index.user.js
@@ -11,31 +11,74 @@
 (function () {
     const EXTENSION_NAME = 'mrgrd56/reverso-context-unblocker';
 
-    try {
-        document.getElementById('blocked-results-banner')?.remove();
-        document.getElementById('blocked-rude-results-banner')?.remove();
+    const onNodeAdded = (handler) => {
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (!mutation.addedNodes) {
+                    return;
+                }
 
-        const examplesContainer = document.getElementById('examples-content');
-        if (!examplesContainer) {
-            return;
-        }
+                for (const addedNode of mutation.addedNodes) {
+                    const isHandled = handler(addedNode);
+                    if (isHandled) {
+                        observer.disconnect();
+                    }
+                }
+            })
+        })
 
-        const examples = examplesContainer.querySelectorAll('.example.blocked');
-        if (!examples.length) {
-            return;
-        }
+        observer.observe(document.body, {
+            childList: true
+            , subtree: true
+            , attributes: false
+            , characterData: false
+        })
+    };
 
-        examples.forEach(example => {
-            example.classList.remove('blocked');
-            if (example.display === 'none') {
-                example.style.display = null;
+    const unblockExamples = () => {
+        try {
+            document.getElementById('blocked-results-banner')?.remove();
+            document.getElementById('blocked-rude-results-banner')?.remove();
+
+            const examplesContainer = document.getElementById('examples-content');
+            if (!examplesContainer) {
+                return;
             }
-            if (example.getAttribute('style') === '') {
-                example.removeAttribute('style');
+
+            const examples = examplesContainer.querySelectorAll('.example.blocked');
+            if (!examples.length) {
+                return;
             }
-        });
-    } catch (e) {
-        console.error(`An error occurred in script '${EXTENSION_NAME}'`)
-        throw e;
+
+            examples.forEach(example => {
+                example.classList.remove('blocked');
+                if (example.display === 'none') {
+                    example.style.display = null;
+                }
+                if (example.getAttribute('style') === '') {
+                    example.removeAttribute('style');
+                }
+            });
+        } catch (e) {
+            console.error(`An error occurred in script '${EXTENSION_NAME}'`)
+            throw e;
+        }
+    };
+
+    if (document.getElementById('blocked-results-banner')) {
+        unblockExamples();
+        return;
     }
+
+    onNodeAdded((addedNode) => {
+        if (addedNode.id !== 'blocked-results-banner') {
+            return false;
+        }
+
+        try {
+            return true;
+        } finally {
+            unblockExamples();
+        }
+    });
 })();
