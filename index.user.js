c9a25d87c744f376c03a649ceceae52c7f1b6a96
k.golikov <kgolikov@37bytes.com>
2022-09-07 18:41:06 +0500

    Done

--- index.user.js
+++ index.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Remove blocking banner showing to unauthorized users from Reverso Context
+// @name         Removes the blocking banner showing to unauthorized users on Reverso Context
 // @namespace    https://context.reverso.net
 // @version      1.0
 // @author       MRGRD56
@@ -8,4 +8,29 @@
 // @grant        none
 // ==/UserScript==
 
-// TODO remove the banner, show hidden and blurred elements
+(function () {
+    const EXTENSION_NAME = 'mrgrd56/reverso-context-unblocker';
+
+    document.getElementById('blocked-results-banner')?.remove();
+    document.getElementById('blocked-rude-results-banner')?.remove();
+
+    const examplesContainer = document.getElementById('examples-content');
+    if (!examplesContainer) {
+        console.error(`[${EXTENSION_NAME}] unable to find '#examples-content' element, so could not show and unblur the hidden examples`);
+        return;
+    }
+
+    const examples = examplesContainer.querySelectorAll('.example.blocked');
+    if (!examples.length) {
+        console.error(`[${EXTENSION_NAME}] unable to find '.example.blocked' elements, so could not show and unblur the hidden examples`);
+        return;
+    }
+
+    examples.forEach(example => {
+        example.classList.remove('blocked');
+        example.style.display = null;
+        if (example.getAttribute('style') === '') {
+            example.removeAttribute('style');
+        }
+    });
+})();
